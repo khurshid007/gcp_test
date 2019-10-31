7ecb3081b8a0472e54f2f27519f76ea4e1a62d2a
khurshid <khurshid_alam007@live.com>
2019-10-31 16:28:49 +0530

    das

--- GCP_Test/Models/Test.cs
+++ GCP_Test/Models/Test.cs
@@ -10,5 +10,6 @@ namespace GCP_Test.Models
         public int MyProperty { get; set; }
         public int MyProperty1 { get; set; }
         public int MyProperty23 { get; set; }
+        public int MyProperty45 { get; set; }
     }
 }
